9ad7255a01c291e70f7056fdb4202827a220fb5b
Michael Wan <michaelwan@berkeley.edu>
2021-08-03 21:21:56 -0400

    Add FAN prediction text and image outputs

--- AI_models.py
+++ AI_models.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import glob
+import shutil
 import os
 
 from Models.Hourglass.inference import hg_labeler
@@ -82,6 +84,7 @@ def FAN_model(resource, model):
     for files in types:
         files_grabbed.extend(glob.glob(os.path.join(resource, files)))
     im_list = sorted(files_grabbed)
+    
     for i in range(len(preds)):
         sp_kpts = []
         frames_name.append(im_list[i])
@@ -96,5 +99,32 @@ def FAN_model(resource, model):
     predictions = {}
     predictions['all_keyps'] = [[], frames_kpts]
     predictions['all_boxes'] = [[] for i in range(len(frames_kpts))]
+    predictions['all_names'] = frames_name
 
+    print('Saving predictions to pickle archive')
     helpers.savepkl(predictions, resource, model)
+    
+    # We create a new directory ("-FAN") containing text files
+    # with coordinates of the predicted landmarks, as well 
+    # image files with the predicted landmarks painted onto
+    # the faces. This allows for manual inspection or external
+    # manipulation of the predictions.
+    if os.path.exists(resource + '-FAN'):
+        shutil.rmtree(resource + '-FAN')
+    os.makedirs(resource + '-FAN')
+    for i, image_file in enumerate(predictions['all_names']):
+        predicted_keypoints = predictions['all_keyps'][1][i][0]
+        print('Saving predictions in image and text formats for ' 
+              + os.path.split(image_file)[1])
+        image = plt.imread(image_file)
+        fig, ax = plt.subplots()
+        ax.imshow(image)
+        ax.plot(predicted_keypoints[0], predicted_keypoints[1],
+                'ro', markersize = 2)
+        base_path = os.path.join(resource + '-FAN',
+                                 os.path.splitext(os.path.basename(image_file))[0])
+        plt.savefig(base_path + '.jpg')
+        with open(base_path + '.txt', 'w') as output_text:
+            output_text.write('file: ' + os.path.split(image_file)[1] + '\n')
+            output_text.write('x: ' + str(list(predicted_keypoints[0])) + '\n')
+            output_text.write('x: ' + str(list(predicted_keypoints[1])) + '\n')
